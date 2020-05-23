7a54b041cdaf687e37b2ce3122ab602ee610d153
Maurílio B. Martins <maurilio.martins@tcsindustrial.com.br>
2020-05-22 23:56:19 -0300

    melhorando codigo de validação do token

--- src/services/validaToken.js
+++ src/services/validaToken.js
@@ -3,13 +3,15 @@ var jwt = require('jsonwebtoken');
 module.exports = app => {
     app.validaToken = (req, res, next) => {
         let token = req.headers.authorization
+
         jwt.verify(token, process.env.SECRET, (err, decoded) => {
             if (err)
-                return res.status(500).send({ auth: false, message: 'Falha na autenticação por token.' });
-
+                return res.status(500).send({
+                    auth: false,
+                    message: 'Falha na autenticação por token.'
+                });
             req.userId = decoded.id;
             next();
         });
-
     }
 }
